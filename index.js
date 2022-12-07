1f5f48b27d32e273fe04d262b770468b460f81d2
Andrew Kibiwott <kibiandrew@gmail.com>
2022-12-07 16:09:43 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+//removing tag mai
+const element = document.getElementById("main");
+element.remove("main");
+const newHeader = document.createElement("h1");
+//creating h1 id
+newHeader.id= "victory"
+//creating innerHtml inside h1
+newHeader.innerHTML = "YOUR-NAME is the champion" 
+
+/*main.remove();
+let newHeader=document.createElement("h1");
+newHeader.id="victory"
+newHeader.innerHTML="YOUR-NAME is the champion"*/
